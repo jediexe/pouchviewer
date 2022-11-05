2c75e49c80ee83c5b1b5b354810339401cfc8079
Jedibrine <jedibrine@gmail.com>
2022-11-05 16:15:30 -0700

    1.5
    1.5 config

--- src/main/java/com/jediexe/pouchviewer/Main.java
+++ src/main/java/com/jediexe/pouchviewer/Main.java
@@ -1,6 +1,10 @@
 package com.jediexe.pouchviewer;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -11,11 +15,23 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class Main{
 	
     public static final String MODID = "pouchviewer";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.5";
+    
+    public static boolean showOwned;
+    public static boolean showDyed;
+    
+    public void initConfiguration(FMLInitializationEvent event) {
+    	Configuration config = new Configuration(new File("config/pouchviewer.cfg"));
+    	config.load();
+    	showOwned = config.getBoolean("showOwned", "config", false, "Set to true to keep the 'Belonged to:' text in the tooltip");
+    	showDyed = config.getBoolean("showDyed", "config", false, "Set to true to keep the 'Dyed' text in the tooltip");
+    	config.save();
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event){
-    	MinecraftForge.EVENT_BUS.register(Draw.instance);
+    	this.initConfiguration(event);
+    	MinecraftForge.EVENT_BUS.register(Pouchviewer.instance);
     }
     
 }
